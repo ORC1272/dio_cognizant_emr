2867c1813821dd0cfcfa91eb136853b52d7b484d
ORC1272 <88564697+ORC1272@users.noreply.github.com>
2021-08-25 19:06:03 -0300

    cognizant emr

---
+++ scripts/script-docker-executar-mrjob.sh
@@ -0,0 +1,4 @@
+python3 mr-analise-palavras-livro.py \
+    -r emr s3://orc-dio-live-datalake-prod/data/TheAdventuresOfSherlockHolmes-ArthurConanDoyle.txt \
+    --output-dir=s3://orc-dio-live-datalake-prod/output/\
+    --cloud-tmp-dir=s3://orc-dio-live-datalake-prod/temp/
